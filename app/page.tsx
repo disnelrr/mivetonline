8228ef42bd084373f2afdf0536986b6c63cadca8
Sule98 <suleidis1998@gmail.com>
2023-09-07 17:07:15 -0400

    Subiendo el header

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,7 @@
-import Image from 'next/image'
+import Header from '@/components/Header/Header';
 
 export default function Home() {
   return (
-
-    <header>
-      <div>
-        <nav className="flex justify-between items-center  bg-primary ">
-          <div className="text-white font-bold ml-5">
-            <div className="flex items-center ml-2">
-              <a href="#" className="mr-4">Horario</a>
-              <a href="#" className="mr-4">Zona </a>
-              <a href="#" className="">Tarifas</a>
-            </div>
-          </div>
-          <div className="flex items-center bg-white w-8/12 py-3 text-center " >
-            <p className='ml-96 '>626 322 230</p>
-          </div>
-          <div className="flex items-center text-white mr-9">
-            <a href="#" className="mr-4"><i className="fab fa-twitter"></i></a>
-            <a href="#" className="mr-4"><i className="fab fa-facebook"></i></a>
-            <a href="#" className="mr-4"><i className="fab fa-linkedin"></i></a>
-            <a href="#" className="mr-4"><i className="fab fa-instagram"></i></a>
-            <a href="#" className="mr-4"><i className="fab fa-youtube"></i></a>
-
-          </div>
-
-
-
-        </nav>
-      </div>
-    </header>
-
-
-
+    <Header />
   )
 }
---
+++ components/Header/HeaderMovile.tsx
@@ -0,0 +1,20 @@
+import MenuMovile from "./MenuMovile";
+
+
+const HeaderMovile = () => {
+    return (
+        <div className=' lg:hidden'>
+            <div className='relative flex justify-between bg-primary text-white font-bold text-center h-12'>
+                <div>MIVETONLINE</div>
+                <div><MenuMovile/></div>
+                </div>
+
+
+
+
+        </div>
+
+    )
+}
+
+export default HeaderMovile;
