728b12418f7ca0ef0593baf53d37af4a9c845bef
Sule98 <suleidis1998@gmail.com>
2023-09-14 13:19:27 -0400

    Centered brand text

--- components/Header/HeaderMovile.tsx
+++ components/Header/HeaderMovile.tsx
@@ -4,8 +4,8 @@ import MenuMovile from "./MenuMovile";
 const HeaderMovile = () => {
     return (
         <div className=' lg:hidden'>
-            <div className='relative flex justify-between bg-primary text-white font-bold text-center h-12'>
-                <div>MIVETONLINE</div>
+            <div className='relative flex justify-between bg-primary text-white font-bold text-center h-12 '>
+                <div className="p-3 ml-3">MIVETONLINE</div>
                 <div><MenuMovile/></div>
                 </div>
 
