4d605371741b1a0f539ae66b15ff294dd04cfa3a
Sule98 <suleidis1998@gmail.com>
2023-10-23 21:02:51 -0400

    Create banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Banner from '@/components/Banner/Banner';
 import Header from '@/components/Header/Header';
+
+
 
 export default function Home() {
   return (
+    <div>
+
     <Header />
+    <Banner />
+    
+
+
+    </div>
+      
+  
+  
   )
 }
