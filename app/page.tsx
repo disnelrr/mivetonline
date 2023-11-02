5dacbd018ab2808f51d7a0502d1c62e677b95ec5
Sule98 <106923508+Sule98@users.noreply.github.com>
2023-11-02 10:42:07 -0400

    Center phone number (#21)
    * Center phone number
    
    * centered phone number

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,33 @@ export default function Home() {
   return (
     <div>
 
-    <Header />
-    <Banner />
-    
+    <header>
+      <div>
+        <nav className="flex justify-between items-center  bg-primary ">
+          <div className="text-white font-bold ml-5">
+            <div className="flex items-center ml-2">
+              <a href="#" className="mr-4">Horario</a>
+              <a href="#" className="mr-4">Zona </a>
+              <a href="#" className="">Tarifas</a>
+            </div>
+          </div>
+          <div className="flex items-center bg-white w-8/12 py-3 justify-center" ><p>626 322 230</p>
+          </div>
+          <div className="flex items-center text-white mr-9">
+            <a href="#" className="mr-4"><i className="fab fa-twitter"></i></a>
+            <a href="#" className="mr-4"><i className="fab fa-facebook"></i></a>
+            <a href="#" className="mr-4"><i className="fab fa-linkedin"></i></a>
+            <a href="#" className="mr-4"><i className="fab fa-instagram"></i></a>
+            <a href="#" className="mr-4"><i className="fab fa-youtube"></i></a>
+
+          </div>
+
+
+
+        </nav>
+      </div>
+    </header>
+
 
 
     </div>
